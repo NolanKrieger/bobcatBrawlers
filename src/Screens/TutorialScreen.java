4016bbcc3d89b35cd9961faafeb0f3c0b04244fe
Lucas Deutsch <lucasdeutsch@Lucass-MacBook-Pro.local>
2025-09-24 11:15:40 -0400

    Tutorial Arrow Description

--- src/Screens/TutorialScreen.java
+++ src/Screens/TutorialScreen.java
@@ -10,6 +10,7 @@ import SpriteFont.SpriteFont;
 
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 
 public class TutorialScreen extends Screen {
@@ -21,8 +22,15 @@ public class TutorialScreen extends Screen {
    protected SpriteFont movementDescription;
    protected SpriteFont attackLabel;
    protected SpriteFont underlinedLabel;
+   protected SpriteFont rightArrowDescription;
+   protected SpriteFont leftArrowDescription;
+   protected SpriteFont downArrowDescription;
+   protected SpriteFont upArrowDescription;
    protected SpriteFont returnInstructionsLabel;
-
+   protected BufferedImage rightArrowImage;
+   protected BufferedImage leftArrowImage;
+   protected BufferedImage downArrowImage;
+   protected BufferedImage upArrowImage;
 
    public TutorialScreen(ScreenCoordinator screenCoordinator) {
        this.screenCoordinator = screenCoordinator;
@@ -39,8 +47,24 @@ public class TutorialScreen extends Screen {
        movementDescription = new SpriteFont("Use Arrow Keys: ", 20, 135, "Times New Roman", 20, Color.white);
        attackLabel = new SpriteFont("Attacks", 550, 100, "Times New Roman",26, Color.white);
        underlinedLabel = new SpriteFont("_______", 350, 5, "Times New Roman", 30, Color.white);
+       rightArrowDescription = new SpriteFont("- Moves you right", 50, 170, "Times New Roman", 18, Color.white);
+       leftArrowDescription = new SpriteFont("- Moves you left", 50, 202, "Times New Roman", 18, Color.white);
+       downArrowDescription = new SpriteFont("- Moves you down", 50, 233, "Times New Roman", 18, Color.white);
+       upArrowDescription = new SpriteFont("- Moves you up", 50, 262, "Times New Roman", 18, Color.white);
        returnInstructionsLabel = new SpriteFont("Press Space to return to the menu", 20, 540, "Times New Roman", 18, Color.white);
        keyLocker.lockKey(Key.SPACE);
+
+       try {
+            rightArrowImage = ImageLoader.load("New Piskel-1.png (3).png");
+            leftArrowImage = ImageLoader.load("New Piskel-1.png (1).png");
+            downArrowImage = ImageLoader.load("New Piskel-1.png (2).png");
+            upArrowImage = ImageLoader.load("New Piskel-1.png.png");
+       } catch (RuntimeException e) {
+            rightArrowImage = null;
+            leftArrowImage = null;
+            downArrowImage = null;
+            upArrowImage = null;
+       }
    }
 
 
@@ -65,8 +89,32 @@ public class TutorialScreen extends Screen {
        tutorialLabel.draw(graphicsHandler);
        movementLabel.draw(graphicsHandler);
        movementDescription.draw(graphicsHandler);
+       if (rightArrowImage != null) {
+        int arrowX = 4;
+        int arrowY = 160;
+        graphicsHandler.drawImage(rightArrowImage, arrowX, arrowY, 50, 50);
+       }
+       if (leftArrowImage != null) {
+        int arrowX = 15;
+        int arrowY = 187;
+        graphicsHandler.drawImage(leftArrowImage, arrowX, arrowY, 50, 50);
+       }
+       if (downArrowImage != null) {
+        int arrowX = 7;
+        int arrowY = 214;
+        graphicsHandler.drawImage(downArrowImage, arrowX, arrowY, 50, 50);
+       }
+       if (upArrowImage != null) {
+        int arrowX = 12;
+        int arrowY = 255;
+        graphicsHandler.drawImage(upArrowImage, arrowX, arrowY, 50, 50);
+       } 
        attackLabel.draw(graphicsHandler);
        underlinedLabel.draw(graphicsHandler);
+       rightArrowDescription.draw(graphicsHandler);
+       leftArrowDescription.draw(graphicsHandler);
+       downArrowDescription.draw(graphicsHandler);
+       upArrowDescription.draw(graphicsHandler);
        returnInstructionsLabel.draw(graphicsHandler);
    }
 }
