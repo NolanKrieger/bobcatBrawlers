edc2e8a75317df56b093c97904130d9baac7e0cb
Nolan Krieger <prokaduray@gmail.com>
2025-10-31 02:54:13 -0400

    added double jump to boomer

--- src/Players/Boomer.java
+++ src/Players/Boomer.java
@@ -3,16 +3,19 @@ package Players;
 import Builders.FrameBuilder;
 import Engine.GraphicsHandler;
 import Engine.ImageLoader;
+import Engine.Keyboard;
 import GameObject.Frame;
 import GameObject.ImageEffect;
 import GameObject.SpriteSheet;
 import Level.Player;
+import Utils.AirGroundState;
 
 import java.util.HashMap;
 
 // This is the class for the Cat player character
 // basically just sets some values for physics and then defines animations
 public class Boomer extends Player {
+    protected int jumpsRemaining = 1; // Double jump - 1 extra jump available
 
     public Boomer(float x, float y) {
         super(new SpriteSheet(ImageLoader.load("boomersprite1.png"), 24, 24), x, y, "STAND_RIGHT");
@@ -28,6 +31,28 @@ public class Boomer extends Player {
 
     public void update() {
         super.update();
+    }
+
+    @Override
+    protected void playerJumping() {
+        // Allow double jump logic - check if a second jump is pressed while in air
+        if (Keyboard.isKeyDown(JUMP_KEY) && !keyLocker.isKeyLocked(JUMP_KEY) && jumpsRemaining > 0 && airGroundState == AirGroundState.AIR) {
+            // Second jump is pressed while in air
+            keyLocker.lockKey(JUMP_KEY);
+            jumpsRemaining--;
+            jumpForce = jumpHeight;
+            momentumY = 0; // Reset momentum so the jump feels fresh
+        }
+
+        // Call parent jump logic for normal jump behavior
+        super.playerJumping();
+    }
+
+    @Override
+    protected void playerStanding() {
+        // Reset jumps when touching the ground
+        jumpsRemaining = 1;
+        super.playerStanding();
     }
 
     public void draw(GraphicsHandler graphicsHandler) {
