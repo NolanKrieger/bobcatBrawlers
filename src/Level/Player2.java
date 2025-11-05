6498884328777b0eec253c018baeb65f2ae7cda2
asgoodman224 <asgoodman@quinnipiac.edu>
2025-11-04 19:12:13 -0500

    Changed cooldown to 7 hits

--- src/Level/Player2.java
+++ src/Level/Player2.java
@@ -64,7 +64,7 @@ public abstract class Player2 extends GameObject {
     protected int disablePressCount = 0;
     protected boolean lastAttackKeyDown = false; // detect key-down edges
     protected int forcedDisableMs = 0; // remaining ms for forced disable
-    protected static final int DISABLE_PRESS_LIMIT = 5;
+    protected static final int DISABLE_PRESS_LIMIT = 7;
     protected static final int FORCED_DISABLE_DURATION_MS = 5000; // 8 seconds
     protected int maxHealth = 5; // number of hits the player can take before dying
     protected int health = maxHealth;
