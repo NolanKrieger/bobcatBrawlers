cec81a88e28cf4d51a2fb3634e1b7084d806d407
Lucas Deutsch <lucasdeutsch@Lucass-MacBook-Pro.local>
2025-11-12 17:33:42 -0500

    Tutorial Format

--- src/Screens/TutorialScreen.java
+++ src/Screens/TutorialScreen.java
@@ -9,12 +9,9 @@ import Players.Cat;
 import Level.Player;
 import Level.PlayerListener;
 import GameObject.GameObject;
-import Maps.TitleScreenMap;
 import SpriteFont.SpriteFont;
-import Screens.TutorialLoseScreen;
 
 import java.awt.*;
-import java.awt.image.BufferedImage;
 
 
 public class TutorialScreen extends Screen implements PlayerListener {
@@ -32,16 +29,8 @@ public class TutorialScreen extends Screen implements PlayerListener {
    protected SpriteFont underlinedLabel1;
    protected SpriteFont underlinedLabel2;
    protected SpriteFont underlinedLabel3;
-   protected SpriteFont rightArrowDescription;
-   protected SpriteFont leftArrowDescription;
-   protected SpriteFont downArrowDescription;
-   protected SpriteFont upArrowDescription;
    protected SpriteFont keyDescription;
    protected SpriteFont returnInstructionsLabel;
-   protected BufferedImage rightArrowImage;
-   protected BufferedImage leftArrowImage;
-   protected BufferedImage downArrowImage;
-   protected BufferedImage upArrowImage;
    protected TutorialLoseScreen tutorialLoseScreen;
    protected boolean showingLevelLose = false;
 
@@ -85,32 +74,16 @@ public class TutorialScreen extends Screen implements PlayerListener {
        this.player = new Cat(background.getPlayerStartPosition().x, background.getPlayerStartPosition().y);
        this.player.setMap(background);
        this.player.addListener(this);
-       tutorialLabel = new SpriteFont("Tutorial", 350, 5, "Times New Roman", 30, Color.white);
-       movementLabel = new SpriteFont("Movements", 20, 40, "Times New Roman", 26, Color.white);
-       movementDescription = new SpriteFont("Use Arrow Keys: ", 20, 78, "Times New Roman", 20, Color.white);
-       attackLabel = new SpriteFont("Attacks", 218, 40, "Times New Roman",26, Color.white);
-       underlinedLabel1 = new SpriteFont("_______", 350, 5, "Times New Roman", 30, Color.white);
-       underlinedLabel2 = new SpriteFont("________", 20, 38, "Times New Roman", 30, Color.white);
-       underlinedLabel3 = new SpriteFont("______", 217, 38, "Times New Roman", 30, Color.white);
-       rightArrowDescription = new SpriteFont("- Moves you right", 50, 107, "Times New Roman", 18, Color.white);
-       leftArrowDescription = new SpriteFont("- Moves you left", 50, 134, "Times New Roman", 18, Color.white);
-       downArrowDescription = new SpriteFont("- Moves you down", 50, 161, "Times New Roman", 18, Color.white);
-       upArrowDescription = new SpriteFont("- Moves you up", 50, 188, "Times New Roman", 18, Color.white);
-       keyDescription = new SpriteFont("-Use WASD to move and jump", 20, 217, "Times New Roman", 18, Color.white);
-       returnInstructionsLabel = new SpriteFont("Press Space to return to the menu", 20, 540, "Times New Roman", 18, Color.white);
+       tutorialLabel = new SpriteFont("Tutorial", 600, 5, "Times New Roman", 30, Color.white);
+       movementLabel = new SpriteFont("Movements", 250, 40, "Times New Roman", 26, Color.white);
+       movementDescription = new SpriteFont("Use WASD: ", 250, 78, "Times New Roman", 20, Color.white);
+       attackLabel = new SpriteFont("Attacks", 950, 40, "Times New Roman",26, Color.white);
+       underlinedLabel1 = new SpriteFont("_______", 600, 5, "Times New Roman", 30, Color.white);
+       underlinedLabel2 = new SpriteFont("________", 250, 38, "Times New Roman", 30, Color.white);
+       underlinedLabel3 = new SpriteFont("______", 950, 38, "Times New Roman", 30, Color.white);
+       keyDescription = new SpriteFont("-Use WASD to move and jump", 250, 107, "Times New Roman", 18, Color.white);
+       returnInstructionsLabel = new SpriteFont("Press Space to return to the menu", 20, 740, "Times New Roman", 18, Color.white);
        keyLocker.lockKey(Key.SPACE);
-
-       try {
-            rightArrowImage = ImageLoader.load("New Piskel-1.png (3).png");
-            leftArrowImage = ImageLoader.load("New Piskel-1.png (1).png");
-            downArrowImage = ImageLoader.load("New Piskel-1.png (2).png");
-            upArrowImage = ImageLoader.load("New Piskel-1.png.png");
-       } catch (RuntimeException e) {
-            rightArrowImage = null;
-            leftArrowImage = null;
-            downArrowImage = null;
-            upArrowImage = null;
-       }
    }
 
 
@@ -145,34 +118,10 @@ public class TutorialScreen extends Screen implements PlayerListener {
        tutorialLabel.draw(graphicsHandler);
        movementLabel.draw(graphicsHandler);
        movementDescription.draw(graphicsHandler);
-       if (rightArrowImage != null) {
-        int arrowX = 4;
-        int arrowY = 95;
-        graphicsHandler.drawImage(rightArrowImage, arrowX, arrowY, 50, 50);
-       }
-       if (leftArrowImage != null) {
-        int arrowX = 15;
-        int arrowY = 118;
-        graphicsHandler.drawImage(leftArrowImage, arrowX, arrowY, 50, 50);
-       }
-       if (downArrowImage != null) {
-        int arrowX = 7;
-        int arrowY = 142;
-        graphicsHandler.drawImage(downArrowImage, arrowX, arrowY, 50, 50);
-       }
-       if (upArrowImage != null) {
-        int arrowX = 12;
-        int arrowY = 181;
-        graphicsHandler.drawImage(upArrowImage, arrowX, arrowY, 50, 50);
-       } 
        attackLabel.draw(graphicsHandler);
        underlinedLabel1.draw(graphicsHandler);
        underlinedLabel2.draw(graphicsHandler);
        underlinedLabel3.draw(graphicsHandler);
-       rightArrowDescription.draw(graphicsHandler);
-       leftArrowDescription.draw(graphicsHandler);
-       downArrowDescription.draw(graphicsHandler);
-       upArrowDescription.draw(graphicsHandler);
        keyDescription.draw(graphicsHandler);
        returnInstructionsLabel.draw(graphicsHandler);
    }
